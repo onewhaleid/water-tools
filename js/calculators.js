223481f53bbf3f6c1f40d7f61bad08c9c38d11b2
Dan Howe <dan.howe.mail@gmail.com>
2018-01-17 18:00:25 +1100

    Add hunt wavelength calculator

--- js/calculators.js
+++ js/calculators.js
@@ -32,5 +32,38 @@ function godaCalculateBreakerIndex(form) {
 
     var result =  0.12 / (d_b / L_0) * (1 - Math.exp(-1.5 * pi * (d_b / L_0) * (1 + 11 * Math.pow(s, 4 / 3))));
     form.gamma.value = Math.round(result * 100) / 100;
+}
 
+function huntCalculateWavelength(form) {
+    var T = parseFloat(form.T.value);
+    var d = parseFloat(form.d.value);
+
+    var pi = Math.PI;
+    var w = 2 * pi / T;
+    var g = 9.8;
+
+    var a1 = 0.666;
+    var a2 = 0.355;
+    var a3 = 0.1608465608;
+    var a4 = 0.063209;
+    var a5 = 0.0217540484;
+    var a6 = 0.0065407983;
+
+    var y = Math.pow(w, 2) * d / g;
+    var y1 = a1 * y + a2 * Math.pow(y, 2) + a3 * Math.pow(y, 3) + a4 * Math.pow(y, 4) + a5 * Math.pow(y, 5) + a6 * Math.pow(y, 6);
+    var y2 = Math.pow(y, 2) + y / (1 + y1);
+
+    var L_0 = 2 * pi * d / Math.sqrt(y2);
+    form.L.value = Math.round(L_0 * 10) / 10
+    form.H_max.value = Math.round(L_0 / 7 * 10) / 10
+
+    var depth_ratio = d / L_0
+    var regime = "intermediate"
+    if (depth_ratio > 0.5){
+      regime = "deep";
+    }
+    if (depth_ratio < 0.05){
+      regime = "shallow";
+    }
+    form.regime.value = regime
 }
