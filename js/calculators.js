af72c66cc4cf21577532024ebd35ee3ff348c5b5
Dan Howe <dan.howe.mail@gmail.com>
2018-01-17 17:52:09 +1100

    Add Goda breaker index calculator

--- js/calculators.js
+++ js/calculators.js
@@ -1,9 +1,3 @@
-function matchInput() {
-  var input1 = document.getElementById("input1");
-  var input2 = document.getElementById("input2");
-  input2.value = input1.value;
-}
-
 function hudsonCalculateHeight(form) {
     var M = parseFloat(form.M.value);
     var rho_a = parseFloat(form.rho_a.value);
@@ -26,4 +20,17 @@ function hudsonCalculateMass(form) {
 
     result = rho_a * Math.pow(H, 3) / (k_d * Math.pow(delta, 3) * slope);
     form.M.value = Math.round(result);
+}
+
+function godaCalculateBreakerIndex(form) {
+    var T = parseFloat(form.T.value);
+    var d_b = parseFloat(form.d_b.value);
+    var s = parseFloat(form.s.value);
+    var g = 9.8;
+    var pi = Math.PI;
+    var L_0 = g * Math.pow(T, 2) / (2 * pi);
+
+    var result =  0.12 / (d_b / L_0) * (1 - Math.exp(-1.5 * pi * (d_b / L_0) * (1 + 11 * Math.pow(s, 4 / 3))));
+    form.gamma.value = Math.round(result * 100) / 100;
+
 }
